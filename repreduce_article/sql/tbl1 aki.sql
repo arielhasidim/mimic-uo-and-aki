24960760d7266ed81cfa2187b57d7e8a7500f558
Ariel <23483971+arielhasidim@users.noreply.github.com>
2023-08-04 15:24:43 +0300

    update "table 1" (actually numbered table 2)

--- repreduce_article/sql/tbl1 aki.sql
+++ repreduce_article/sql/tbl1 aki.sql
@@ -1,9 +1,21 @@
-SELECT a.stay_id, 
-  MAX(a.aki_stage_uo) max_stage, 
-  IF(MAX(a.aki_stage_uo) = 0, 0, 1) aki_binary,
-  COUNT(b.AKI_ID) aki_count
-FROM `mimic_uo_and_aki.d3_kdigo_stages` a
-LEFT JOIN mimic_uo_and_aki.e_aki_analysis b
-  ON b.STAY_ID = a.stay_id
-WHERE a.aki_stage_uo IS NOT NULL
-GROUP BY a.stay_id
+WITH kdigo AS (
+  SELECT stay_id,
+  MAX(aki_stage_uo) max_stage,
+  IF(MAX(aki_stage_uo) = 0, 0, 1) aki_binary,
+  COUNTIF(aki_stage_uo>0) / COUNT(aki_stage_uo) aki_portion
+  FROM `mimic_uo_and_aki.d3_kdigo_stages` a
+  WHERE a.aki_stage_uo IS NOT NULL
+  GROUP BY stay_id
+)
+
+SELECT kdigo.stay_id, 
+  kdigo.max_stage, 
+  kdigo.aki_binary,
+  kdigo.aki_portion,
+  COUNT(a.AKI_ID) aki_count,
+  AVG(DATETIME_DIFF(a.AKI_STOP, a.AKI_START, MINUTE) / 60) aki_mean_duration,
+  SUM(DATETIME_DIFF(a.AKI_STOP, a.AKI_START, MINUTE) / 60 / 24) aki_total_time
+FROM kdigo
+LEFT JOIN `mimic_uo_and_aki.e_aki_analysis` a
+  ON a.STAY_ID = kdigo.stay_id
+GROUP BY kdigo.stay_id, kdigo.max_stage, kdigo.aki_binary, kdigo.aki_portion
